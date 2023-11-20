01db57ede0adbf5e782905d90057bf72d709e755
jsburbano <joans.burbano@gmail.com>
2023-11-19 22:32:51 -0500

    fix: change tag img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
  return (
   <div className=" flex justify-center items-center h-full" style={{height: '100vh'}}>
-    <img className=" absolute z-0 h-full w-full" src="https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/tenistide/bg-tt.png" alt="" />
+    <Image className=" absolute z-0 h-full w-full" src="https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/tenistide/bg-tt.png" alt="" />
     <figure className="c z-10 bg-white p-20 shadow-lg rounded-lg">
-      <img src="https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/tenistide/tt-blue.png" style={{filter: 'drop-shadow(4px 4px 0px #ffff00)'}}  alt="logo" className='w-40 block mb-6' />
+      <Image src="https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/tenistide/tt-blue.png" style={{filter: 'drop-shadow(4px 4px 0px #ffff00)'}}  alt="logo" className='w-40 block mb-6' />
       <p className=" font-extrabold text-white text-3xl mb-6" style={{textShadow: `3px 3px 2px #000, 0 0 2px #000`, userSelect: 'none'}}>Tenis Tide</p>
       <Link href={'access'} className='flex'>
         <button className=' bg-yellow-300 w-40 h-10 rounded-md m-auto shadow-xl'>Access</button>
